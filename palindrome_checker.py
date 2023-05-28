6d2a03c9df401b06a4b95499bb05039394ccde0c
Jordan Needham <jdncreations@hotmail.com>
2023-05-28 17:07:51 +0800

    palindrome checker

---
+++ palindrome_checker.py
@@ -0,0 +1,18 @@
+import sys
+
+
+def check_for_palindrome(string):
+    backwards_string = ""
+    for i in reversed(range(len(string))):
+        backwards_string = backwards_string + string[i]
+
+    if string != backwards_string:
+        return False
+    return True
+
+
+if len(sys.argv) < 2:
+    print('Usage: py pdc.py [text]')
+    sys.exit()
+
+print(check_for_palindrome(sys.argv[1]))
