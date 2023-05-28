6418a4a4c815cea25f2be902f2911a5f958b86aa
Jordan Needham <jdncreations@hotmail.com>
2023-05-28 20:32:20 +0800

    added extra function so you can input amount of repeats

--- repeat_characters.py
+++ repeat_characters.py
@@ -1,3 +1,11 @@
+def repeat_characters_amount(base_string, amount):
+    repeated_string = ""
+    for char in base_string:
+        repeated_string = repeated_string + (char * amount)
+
+    return repeated_string
+
+
 def repeat_characters(base_string):
     repeated_string = ""
     for char in base_string:
@@ -7,3 +15,4 @@ def repeat_characters(base_string):
 
 
 print(repeat_characters("FROGS"))
+print(repeat_characters_amount("frogs", 4))
