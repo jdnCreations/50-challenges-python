8d1c8e51068037d09df687c18db69c9a3d2b446c
Jordan Needham <jdncreations@hotmail.com>
2023-05-28 19:10:25 +0800

    added an extra function so you can check any two characters

--- x_and_o.py
+++ x_and_o.py
@@ -1,14 +1,27 @@
+
+def equal_amount_of_char(string, char1, char2):
+    i = 0
+    j = 0
+    for char in string.lower():
+        if char == char1:
+            i = i + 1
+        if char == char2:
+            j = j + 1
+    return i == j
+
+
 def equal_x_and_o(string):
     os = 0
     xs = 0
     for char in string.lower():
-        if (char == 'x'):
+        if char == 'x':
             xs = xs + 1
-        if (char == 'o'):
+        if char == 'o':
             os = os + 1
     return os == xs
 
 
+print(equal_amount_of_char("XXXOOO", "x", "o"))
 print(equal_x_and_o("XOXOXOXXA"))
 print(equal_x_and_o("XXXOOO"))
 print(equal_x_and_o("xxXXoOXoXOxXXXOo"))
