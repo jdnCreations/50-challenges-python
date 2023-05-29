6eeb347f14532371cac078958311189fb32cec72
Jordan Needham <jdncreations@hotmail.com>
2023-05-29 14:57:54 +0800

    fixed tidy link, automating README links with create_readme.py

--- README.md
+++ README.md
@@ -1,8 +1,46 @@
 # 50 Challenges/Exercise in Python
 
-### Completing 50 challenges/exercises in python for practice :)
+## Completing 50 challenges/exercises in python for practice :)
 
+### Links to all Challenges so far
 
-https://www.codecademy.com/resources/blog/python-code-challenges-for-beginners/
+[10 Python Challenges @ CodeAcademy](https://www.codecademy.com/resources/blog/python-code-challenges-for-beginners/)
 
-https://edabit.com
+[Tidy Hyperlinks](https://edabit.com/challenge/nfPNbisehCjuC8mkd)
+
+[Virtual DAC](https://edabit.com/challenge/AJGqpNL2yAyhbdpvB)
+
+[Stuttering Function](https://edabit.com/challenge/gt9LLufDCMHKMioh2)
+
+[Luke, I Am Your ...](https://edabit.com/challenge/8pDH2SRutPoaQghgc)
+
+[Invert Colors](https://edabit.com/challenge/i6hY9JSjQK4jcaB6i)
+
+[Calculating Damage](https://edabit.com/challenge/HSHHkdRYXfgfZSqri)
+
+[Finding Adjacent Nodes](https://edabit.com/challenge/3DAkZHv2LZjgqWbvW)
+
+[Length of Number](https://edabit.com/challenge/2zKetgAJp4WRFXiDT)
+
+[Burglary Series (03): Is It Gone?](https://edabit.com/challenge/2wQPKcSipXmK4idwD)
+
+[Calculate the Profit](https://edabit.com/challenge/YfoKQWNeYETb9PYpw)
+
+[Return the Time Saved by Speeding](https://edabit.com/challenge/QgSMSMpfcEebAyCye)
+
+[Sum of Resistance in Series Circuits](https://edabit.com/challenge/gzmFeaXwFv8X6pBGq)
+
+[Basic Calculator](https://edabit.com/challenge/ZdnwC3PsXPQTdTiKf)
+
+[Find Domain Name From DNS Pointer (PTR) Records Using IP Address](https://edabit.com/challenge/MtktG9Dz7z9vBCFYM)
+
+[Flipping Bits](https://edabit.com/challenge/z39yXccJGLAy3BDNX)
+
+[The Snake � Area Filling](https://edabit.com/challenge/Y5Ji2HDnQTX7MxeHt)
+
+[Basic Arithmetic Operations on a String Number](https://edabit.com/challenge/peezjw73G8BBGfHdW)
+
+[The Karaca's Encryption Algorithm](https://edabit.com/challenge/JzBLDzrcGCzDjkk5n)
+
+[Pluralize!](https://edabit.com/challenge/LR98GCwLGYPSv8Afb)
+
---
+++ create_readme.py
@@ -0,0 +1,49 @@
+import os
+from tidy_hyperlinks import tidy_link
+import requests
+
+links = [
+    "https://edabit.com/challenge/3DAkZHv2LZjgqWbvW",
+    "https://edabit.com/challenge/Y5Ji2HDnQTX7MxeHt",
+    "https://edabit.com/challenge/nfPNbisehCjuC8mkd",
+    "https://edabit.com/challenge/AJGqpNL2yAyhbdpvB",
+    "https://edabit.com/challenge/SHdu4GwBQehhDm4xT",
+
+]
+
+challenges = {
+    "Tidy Hyperlinks": "https://edabit.com/challenge/nfPNbisehCjuC8mkd",
+    "Virtual DAC": "https://edabit.com/challenge/AJGqpNL2yAyhbdpvB",
+    "Stuttering Function": "https://edabit.com/challenge/gt9LLufDCMHKMioh2",
+    "Luke, I Am Your ...": "https://edabit.com/challenge/8pDH2SRutPoaQghgc",
+    "Invert Colors": "https://edabit.com/challenge/i6hY9JSjQK4jcaB6i",
+    "Calculating Damage": "https://edabit.com/challenge/HSHHkdRYXfgfZSqri",
+    "Finding Adjacent Nodes": "https://edabit.com/challenge/3DAkZHv2LZjgqWbvW",
+    "Length of Number": "https://edabit.com/challenge/2zKetgAJp4WRFXiDT",
+    "Burglary Series (03): Is It Gone?": "https://edabit.com/challenge/2wQPKcSipXmK4idwD",
+    "Calculate the Profit": "https://edabit.com/challenge/YfoKQWNeYETb9PYpw",
+    "Return the Time Saved by Speeding": "https://edabit.com/challenge/QgSMSMpfcEebAyCye",
+    "Sum of Resistance in Series Circuits": "https://edabit.com/challenge/gzmFeaXwFv8X6pBGq",
+    "Basic Calculator": "https://edabit.com/challenge/ZdnwC3PsXPQTdTiKf",
+    "Find Domain Name From DNS Pointer (PTR) Records Using IP Address": "https://edabit.com/challenge/MtktG9Dz7z9vBCFYM",
+    "Flipping Bits": "https://edabit.com/challenge/z39yXccJGLAy3BDNX",
+    "The Snake — Area Filling": "https://edabit.com/challenge/Y5Ji2HDnQTX7MxeHt",
+    "Basic Arithmetic Operations on a String Number": "https://edabit.com/challenge/peezjw73G8BBGfHdW",
+    "The Karaca's Encryption Algorithm": "https://edabit.com/challenge/JzBLDzrcGCzDjkk5n",
+    "Pluralize!": "https://edabit.com/challenge/LR98GCwLGYPSv8Afb",
+}
+
+def add_challenges_to_readme():
+    cwd = os.getcwd()
+    file_name = "README.md"
+
+    file = open(cwd + "\\" + file_name, 'a')
+
+    for key, value in challenges.items():
+        file.write(tidy_link(key, value))
+        file.write('\n\n')
+
+    print("Writing complete.")
+    file.close()
+
+add_challenges_to_readme()
